0e45899a442a0e11efb1385da174c40756daff8c
wyosa <intejie@gmail.com>
2026-03-16 10:42:59 +0200

    added open default system app in neo-tree  hotkey

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,25 +1,37 @@
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = { 'Neotree' },
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-      filtered_items = {
-        visible = true,
-      },
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = { "Neotree" },
+	keys = {
+		{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+	},
+	opts = {
+		filesystem = {
+			commands = {
+				system_open = function(state)
+					local node = state.tree:get_node()
+					local path = node:get_id()
+
+					local _, err = vim.ui.open(path)
+					if err then
+						vim.notify(err, vim.log.levels.ERROR)
+					end
+				end,
+			},
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+					["O"] = "system_open",
+				},
+			},
+			filtered_items = {
+				visible = true,
+			},
+		},
+	},
 }
