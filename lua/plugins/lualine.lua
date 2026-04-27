c553392b37ba21337a46f76949047edc250269e5
wyosa <intejie@gmail.com>
2026-04-27 15:57:48 +0300

    chore: show relative path in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,6 +7,14 @@ return {
 				component_separators = "",
 				section_separators = { left = "", right = "" },
 			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+			},
 			extensions = {
 				"neo-tree",
 				"fzf",
